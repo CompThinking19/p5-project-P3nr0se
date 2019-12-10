d7348703559e8b674b268d328bd4d14f6769d7da
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-12-10 16:27:26 -0500

    Slightly improved eraser
    Still no way to toggle it off

--- sketch.js
+++ sketch.js
@@ -6,21 +6,31 @@ var erasing = false
 function setup() {
 //Basic Canvas Set Up
   createCanvas(400, 400);
+  background(255);
   strokeWeight(5);
+//Creating a toggle button for erasing
+  square(15, 300, 55);
 //This creates a slider that can be used to adjust the color of the drawing tool.
   colorMode(HSB, 255);
   slider = createSlider(0, 255, 127);
 }
 
 function draw() {
+  if (mouseX > -20 &&
+      mouseX < 35 &&
+      mouseY > 122 &&
+      mouseY < 355  &&
+     mouseIsPressed === true) {
+    erasing = true
+  }
 //Defining the tool that is used to draw
   stroke(slider.value(), 255, 255);
   if (mouseIsPressed === true && erasing === false) {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 //Making an eraser tool
- if (mouseIsPressed === true && erasing === true) {
-   stroke(255, 255, 255)
+  if (mouseIsPressed === true && erasing === true) {
+   stroke(255)
    line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
